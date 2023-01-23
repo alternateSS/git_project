957880a5551b4688de7e62cb81a3c03733063137
alternateSS <potryasnyidjek@gmail.com>
2023-01-23 10:34:25 +0600

    First commit2

--- app/items/views.py
+++ app/items/views.py
@@ -1,13 +1,15 @@
+
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
 from .models import Item
 from rest_framework import viewsets
 from .serializers import ItemSerializer
 
 
-class ItemView(viewsets.ModelViewSet):
+class ItemListCreateView(generics.CreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = [IsAuthenticated]
