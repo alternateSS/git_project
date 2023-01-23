0978e28159a7b35c5381b251204f5615910d3ff6
alternateSS <potryasnyidjek@gmail.com>
2023-01-23 10:27:21 +0600

    Authenticated

--- app/items/views.py
+++ app/items/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Item
 from rest_framework import viewsets
 from .serializers import ItemSerializer
@@ -7,4 +10,6 @@ from .serializers import ItemSerializer
 class ItemView(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
 
